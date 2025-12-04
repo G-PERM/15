5798971d1eb4424dc65a5c59ea28d3d4766f156f
G-PERM <gpermweb@gmail.com>
2025-12-04 11:01:48 +0530

    The Formatting

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-display = document.getElementsByClassName("display")[0];
-let i = 1;
-while (i <= 1000) {
-  result = BigInt(2 ** i);
-  display.innerHTML +=
-    '<span style="display: block; margin: 0.5vw 0;">2 to the power of ' +
-    i +
-    " is " +
-    result +
-    "</span><br />";
-  i++;
-}
+display=document.getElementsByClassName("display")[0];let i=1;for(;i<=1e3;)result=BigInt(2**i),display.innerHTML+='<span style="display: block; margin: 0.5vw 0;">2 to the power of '+i+" is "+result+"</span><br />",i++;
