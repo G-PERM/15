f043487f60899f5a44221757cd511710dca600a1
G-PERM <gpermweb@gmail.com>
2026-07-21 17:01:46 +0530

    Initialization

---
+++ script.js
@@ -0,0 +1,1 @@
+display=document.getElementsByClassName("display")[0];let i=1;for(;i<=1e3;)result=BigInt(2**i),display.innerHTML+='<span style="display: block; margin: 0.5vw 0;">2 to the power of '+i+" is "+result+"</span><br />",i++;
